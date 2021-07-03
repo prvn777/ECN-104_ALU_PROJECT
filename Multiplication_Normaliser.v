72926ddd3d0a0187adf871956fe29531548b3539
Praveen nawal <85383637+prvn777@users.noreply.github.com>
2021-07-03 19:07:40 +0530

    Update Multiplication_Normaliser.v

--- Multiplication_Normaliser.v
+++ Multiplication_Normaliser.v
@@ -1,4 +1,4 @@
-module MULTIPLICATION_NORMALISER(in_e, in_m, out_e, out_m);
+module Multiplication_Normaliser(in_e, in_m, out_e, out_m);
   // Inputs
   input [7:0] in_e;
   input [47:0] in_m;
@@ -20,16 +20,20 @@ module MULTIPLICATION_NORMALISER(in_e, in_m, out_e, out_m);
 	  if (in_m[46:41] == 6'b000001) begin
 			out_e = in_e - 5;
 			out_m = in_m << 5;
-		end else if (in_m[46:42] == 5'b00001) begin
+		end 
+	  else if (in_m[46:42] == 5'b00001) begin
 			out_e = in_e - 4;
 			out_m = in_m << 4;
-		end else if (in_m[46:43] == 4'b0001) begin
+		end
+	  else if (in_m[46:43] == 4'b0001) begin
 			out_e = in_e - 3;
 			out_m = in_m << 3;
-		end else if (in_m[46:44] == 3'b001) begin
+		end 
+	  else if (in_m[46:44] == 3'b001) begin
 			out_e = in_e - 2;
 			out_m = in_m << 2;
-		end else if (in_m[46:45] == 2'b01) begin
+		end 
+	  else if (in_m[46:45] == 2'b01) begin
 			out_e = in_e - 1;
 			out_m = in_m << 1;
 		end
