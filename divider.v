7d38ad7565cf9c770b6b923abc9f97e9735521cc
Praveen nawal <85383637+prvn777@users.noreply.github.com>
2021-07-03 18:34:46 +0530

    Create divider.v

---
+++ divider.v
@@ -0,0 +1,25 @@
+module divider (A, B, Output);
+	// Inputs
+	input [31:0] A;
+	input [31:0] B;
+
+
+	output [31:0] Output;
+
+	// Wires
+	wire [31:0] ReciprocalB;
+
+
+	reciprocal recip
+	(
+		.Input(B),
+		.Output(ReciprocalB)
+	);
+
+	multiplier mult
+	(
+		.A(A),
+		.B(ReciprocalB),
+		.Output(Output)
+	);
+endmodule
